f0218680311a667021df1502242ff2601a24546b
Varun <54261591+pathisaivarun@users.noreply.github.com>
2023-04-24 10:37:27 +0530

    Update clean_and_convert_movies_data.py

--- clean_and_convert_movies_data.py
+++ clean_and_convert_movies_data.py
@@ -1,43 +1,42 @@
 import glob
-import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
 def clean_data():
     print("Task started")
+    spark = SparkSession.builder.appName('MyApp').master("local").getOrCreate()
     for file in glob.glob("./movies_data/*.csv"):
         print("File found: {}".format(file))
-        df = pd.read_csv(file, encoding='latin-1')
         filename = file.split("/")[-1]
         print(filename)
-        df["rating"] = pd.to_numeric(df['rating'], errors='coerce')
-        df.drop(df[df['year'].isin(['None'])].index, inplace = True)
-        df = df[df['year'].str.contains('\d{4}')]
-        df["year"] = df['year'].apply(lambda x: x.replace("(", "").replace(")", ""))
-        df['year'] = df['year'].str.replace('\D+', '').apply(lambda x: x[-4:])
-        df['year'] = df['year'].astype("int")
-        df['runtime'] = df["runtime"].apply(lambda x: x.replace("min", "").replace(",", ""))
-        df['runtime'] = df['runtime'].astype("int")
-        df.drop(df[df['runtime'].apply(lambda x: x <= 0) == True].index, inplace = True)
-        df['genre'] = df['genre'].apply(lambda x: x.strip())
-        df['rating'] = df['rating'].astype("float")
-        df['votes'] = df['votes'].apply(lambda x: x.replace(",", ""))
-        df = df[df['votes'].str.contains('^\d+$')]
-        df['votes'] = df['votes'].astype("int")
-        df.drop('director', axis = 1, inplace=True)
-        df.to_csv("./cleaned_movies_data/{}".format(filename), index=False)
-    
-    print("Data cleaning is done")
+        df = spark.read.option("multiLine", "true").csv(file, inferSchema =True, header = True)
+        df = df.drop('director')
+        df = df.withColumn('rating', df["rating"].cast(FloatType()))
+        df = df.filter(("year not in ('', 'None') and year is not NULL"))
+        df = df.filter(("year regexp '[0-9]+'"))
+        df = df.withColumn("year", regexp_replace("year", "\(", ""))
+        df = df.withColumn("year", regexp_replace("year", "\)", ""))
+        df = df.withColumn("year", regexp_replace("year", "\D+", ""))
+        df = df.withColumn('year', substring('year', -4, 4))
+        df = df.withColumn('year', df["year"].cast(IntegerType()))
+        df = df.withColumn('runtime', regexp_replace('runtime', "min", ""))
+        df = df.filter("runtime > 0")
+        df = df.withColumn("votes", regexp_replace('votes', ",", ""))
+        df = df.withColumn('votes', df['votes'].cast(IntegerType()))
+        df.toPandas().to_csv('./cleaned_movies_data/{}'.format(filename), index=False)
 
+        print("Data cleaning is done")
 
 def merge_data():
     full_df = None
+    spark = SparkSession.builder.appName('MyApp').master("local").getOrCreate()
     for file in glob.glob("./cleaned_movies_data/*.csv"):
         print(file)
-        df = pd.read_csv(file)
+        df = spark.read.option("multiLine", "true").csv(file, inferSchema =True, header = True)
         if full_df is None:
             full_df = df
         else:
-            full_df = pd.concat([full_df, df])
-        
-    full_df.to_csv("./cleaned_movies_data/all_movies_data.csv", index=False, sep="|")
+            full_df = full_df.union(df)
+    full_df.toPandas().to_csv('./cleaned_movies_data/all_movies_data.csv', index=False)
     print("Successfully created single csv file for all the data")
-    #ti.xcom_push(key='data_type', value="movies")
